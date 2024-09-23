41b0925fcb6bfa799e1bade3e00c51d921fdb595
Anton Mamontov <a.mamontov@p-s.kz>
2024-09-23 23:27:39 +0500

    vitepwa and webmanifest rout change

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      srcDir: "/",
+      srcDir: "/vueall",
       filename: "sw.ts",
       registerType: 'autoUpdate',
       // injectRegister: 'auto',
