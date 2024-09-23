7193a1589beccf8da9b4aaafba6d6696db1dd6fa
Anton Mamontov <a.mamontov@p-s.kz>
2024-09-23 21:34:30 +0500

    config base route

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  
+
   plugins: [
     vue(),
     VitePWA({
@@ -21,6 +24,8 @@ export default defineConfig({
       injectManifest: {
         injectionPoint: undefined,
       }
-    })
+    }),
+    
   ],
+  base: "/vueall"
 })
